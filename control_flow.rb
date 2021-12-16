544468b6dad6553429d126370458297064ff54f9
Marcus <marcus.shimotsu@gmail.com>
2021-12-15 22:14:49 -0600

    commit tree 6ce75b869badee237cf154989029a9f3e6d749b8

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,44 @@
 def admin_login(username, password)
-  # your code here
+  return username.downcase == "admin" && password == "12345" ? "Access granted" : "Access denied"
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  if(temperature < 40)
+    weather = "It's brisk out there!"
+  elsif(temperature<=65)
+    weather = "It's a little chilly out there!"
+  elsif(temperature>85)
+    weather = "It's too dang hot out there!"
+  else
+    weather = "It's perfect out there!"
+  end
+  return weather
 end
 
 def fizzbuzz(num)
-  # your code here
+  if(num%3==0 && num%5==0)
+    return "FizzBuzz"
+  elsif num%3==0
+    return "Fizz"
+  elsif num%5==0
+    return "Buzz"
+  else
+    return num
+  end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  if operation=="+"
+    return num1+num2
+  elsif operation=="-"
+    return num1-num2
+  elsif operation=="*"
+    return num1*num2
+  elsif operation=="/"
+    return num1/num2
+  else
+    puts "Invalid operation!"
+    return nil
+  end
 end
 
